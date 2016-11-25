a9a10b2ce9a7952edac3404cd2d74cb439e9b326
Dirc <e.e.cornet@gmail.com>
2016-11-25 09:13:26 +0100

    update config class

--- util/config.py
+++ util/config.py
@@ -12,17 +12,21 @@ class Config(object):
     '''
     config = ConfigParser.ConfigParser()
 
-    def __init__(self):
+    def __init__(self, conf_file="conf/config.cfg"):
         '''
         Constructor
-        '''    
-        self.config = ConfigParser.ConfigParser()
-        self.app_base = os.path.dirname(os.path.dirname(__file__))
-        self.config_file = os.path.join(self.app_base,'local_config_template','local_sm9_sync.cfg')
+        '''  
+        self.rel_conf_file   = conf_file  
+        self.config      = ConfigParser.ConfigParser()
+        self.app_base    = os.path.dirname(os.path.dirname(__file__))
+        self.config_file = os.path.join(self.app_base,self.rel_conf_file)
         self.config.read(self.config_file)
         
     def getConfigVar(self, item, key):
         return self.config.get(item,key)
+    
+    def getSections(self):
+        return self.config.sections()
     
     def isDebugEnabled(self):
         return self.config.get('config','debug') == 'on'
@@ -31,7 +35,6 @@ class Config(object):
         return self.app_base, self.config_file
     
     
-    
 if __name__ == '__main__':    
         conf = Config()
-        print conf.config_file
+        print conf.configInfo()
