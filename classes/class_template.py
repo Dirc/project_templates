867b62cfbdbfbf7a1dde168e1034abfb51210f09
Dirc <e.e.cornet@gmail.com>
2016-11-25 13:08:06 +0100

    added classmethod

--- classes/class_template.py
+++ classes/class_template.py
@@ -15,16 +15,23 @@ class SuperClass(object):
         Constructor
         '''
         self.params = params
-        
+    
+    #instancemethod    
     def getParams(self):
         return self.params
     
     @staticmethod
     def classIndependentMethod(x,y):
-        # Method is independent from data of the class
+        # Method is independent from data of the class or instance
         # Note: there is no self parameters
         return x, y
     
+    @classmethod
+    def instanceIndependentMethod(cls):
+        # Method is independent from individual instances
+        # Dependendt from class
+        # Note: the self parameter is changed to the cls parameter.
+        pass
 
 class SubClass(SuperClass):
     def __init__(self, extra_params):
